a6b0ec604573fef29684122e418571b9785b114f
felix <felix@felixdeMacBook-Pro.local>
2025-11-04 22:02:31 +0900

    feat: init

--- resources/remote/paper-home-index/index.js
+++ resources/remote/paper-home-index/index.js
@@ -1,6 +1,6 @@
 System.register("chunks:///_virtual/paper-home-index",["./PaperHomeIndex.ts"],(function(){return{setters:[null],execute:function(){}}}));
 
-System.register("chunks:///_virtual/PaperHomeIndex.ts",["./rollupPluginModLoBabelHelpers.js","cc","./BaseView.ts","./app.ts"],(function(e){var n,o,t,a,r;return{setters:[function(e){n=e.inheritsLoose},function(e){o=e.cclegacy,t=e._decorator},function(e){a=e.default},function(e){r=e.app}],execute:function(){var c;o._RF.push({},"ca4bcX/UNtPC5Vbdc7NhosJ","PaperHomeIndex",void 0);var i=t.ccclass;t.property,e("PaperHomeIndex",i("PaperHomeIndex")(c=function(e){function o(){return e.apply(this,arguments)||this}n(o,e);var t=o.prototype;return t.onLoad=function(){},t.onShow=function(e){},t.onHide=function(e){return e},t.goPageGame=function(){r.manager.ui.show({name:"PageGame"})},t.showPopTip=function(){r.manager.ui.show({name:"PopTip"})},o}(a))||c);o._RF.pop()}}}));
+System.register("chunks:///_virtual/PaperHomeIndex.ts",["cc","./BaseView.ts","./app.ts"],(function(e){var a,o,n,t;return{setters:[function(e){a=e.cclegacy,o=e._decorator},function(e){n=e.default},function(e){t=e.app}],execute:function(){var c;a._RF.push({},"ca4bcX/UNtPC5Vbdc7NhosJ","PaperHomeIndex",void 0);const{ccclass:r,property:s}=o;e("PaperHomeIndex",r("PaperHomeIndex")(c=class extends n{onLoad(){}onShow(e){}onHide(e){return e}goPageGame(){t.manager.ui.show({name:"PageGame"})}showPopTip(){t.manager.ui.show({name:"PopTip"})}})||c);a._RF.pop()}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/paper-home-index', 'chunks:///_virtual/paper-home-index'); 
--- resources/remote/pop-tip/index.js
+++ resources/remote/pop-tip/index.js
@@ -1,6 +1,6 @@
 System.register("chunks:///_virtual/pop-tip",["./PopTip.ts"],(function(){return{setters:[null],execute:function(){}}}));
 
-System.register("chunks:///_virtual/PopTip.ts",["./rollupPluginModLoBabelHelpers.js","cc","./BaseView.ts"],(function(o){var n,t,e,r;return{setters:[function(o){n=o.inheritsLoose},function(o){t=o.cclegacy,e=o._decorator},function(o){r=o.default}],execute:function(){var i;t._RF.push({},"20017Eemr9C9bageJ9He3Li","PopTip",void 0);var u=e.ccclass;e.property,o("PopTip",u("PopTip")(i=function(o){function t(){return o.apply(this,arguments)||this}n(t,o);var e=t.prototype;return e.onLoad=function(){},e.onShow=function(o){return{show:"1"}},e.onHide=function(o){return{hide:"0"}},t}(r))||i);t._RF.pop()}}}));
+System.register("chunks:///_virtual/PopTip.ts",["cc","./BaseView.ts"],(function(e){var o,t,c;return{setters:[function(e){o=e.cclegacy,t=e._decorator},function(e){c=e.default}],execute:function(){var n;o._RF.push({},"20017Eemr9C9bageJ9He3Li","PopTip",void 0);const{ccclass:r,property:s}=t;e("PopTip",r("PopTip")(n=class extends c{onLoad(){}onShow(e){return{show:"1"}}onHide(e){return{hide:"0"}}})||n);o._RF.pop()}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/pop-tip', 'chunks:///_virtual/pop-tip'); 
