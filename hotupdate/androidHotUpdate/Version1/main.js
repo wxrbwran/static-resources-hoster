e9afd7b592036b27ffc67d4d4c6c64d649742aad
felix <felix@felixdeMacBook-Pro.local>
2025-12-09 10:49:44 +0900

    feat: hotupdate

--- hotupdate/androidHotUpdate/Version1/main.js
+++ hotupdate/androidHotUpdate/Version1/main.js
@@ -1,4 +1,38 @@
 
+(function () {
+    if (typeof window.jsb === 'object') {
+        var hotUpdateSearchPaths = localStorage.getItem('HotUpdateSearchPaths');
+        if (hotUpdateSearchPaths) {
+            var paths = JSON.parse(hotUpdateSearchPaths);
+            jsb.fileUtils.setSearchPaths(paths);
+
+            var fileList = [];
+            var storagePath = paths[0] || '';
+            var tempPath = storagePath + '_temp/';
+            var baseOffset = tempPath.length;
+
+            if (jsb.fileUtils.isDirectoryExist(tempPath) && !jsb.fileUtils.isFileExist(tempPath + 'project.manifest.temp')) {
+                jsb.fileUtils.listFilesRecursively(tempPath, fileList);
+                fileList.forEach(srcPath => {
+                    var relativePath = srcPath.substr(baseOffset);
+                    var dstPath = storagePath + relativePath;
+
+                    if (srcPath[srcPath.length] == '/') {
+                        jsb.fileUtils.createDirectory(dstPath)
+                    }
+                    else {
+                        if (jsb.fileUtils.isFileExist(dstPath)) {
+                            jsb.fileUtils.removeFile(dstPath)
+                        }
+                        jsb.fileUtils.renameFile(srcPath, dstPath);
+                    }
+                })
+                jsb.fileUtils.removeDirectory(tempPath);
+            }
+        }
+    }
+})();
+
 
 // SystemJS support.
 window.self = window;
