d48fb5bff36dab63d06a6aaecbb3af48ccd16600
felix <felix@felixdeMacBook-Pro.local>
2025-12-20 16:08:22 +0900

    feat: remote

---
+++ hotupdate/remote/ocean/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/ocean",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/ocean', 'chunks:///_virtual/ocean'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
